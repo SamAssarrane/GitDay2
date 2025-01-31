6b545345d0b94296940479aec65b681b6da22f0f
issam <issam@RPC-2RVWMQ2>
2025-01-31 18:18:43 -0500

    Added more code.

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,7 +5,12 @@ public class DemoDay2 {
 	// Main Method
 	public static void main(String[] args) {
 		
-		System.out.println("Sam");
+		System.out.println("Day 2");
+		
+		int a = 10;
+		int b = 10;
+		
+		System.out.println(a + b);
 	}
 
 }
