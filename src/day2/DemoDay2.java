ce715f7731e02e9d92b72b3e34b65fd79b53b88a
issam <issam@RPC-2RVWMQ2>
2025-01-31 18:47:49 -0500

    Stashing examples..

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -1,18 +1,33 @@
 package day2;
 
 public class DemoDay2 {
-	
+
 	// Main Method
 	public static void main(String[] args) {
-		
+
 		System.out.println("Day 2");
-		
+
 		int a = 10;
 		int b = 10;
-		
+
 		System.out.println(a + b);
 
 		// This is from GitHub.
+	}
+
+	// Example of Stash - Addition Method
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	// Example of Stash - Subtraction Method
+	public void subtract(int a, int b) {
+		System.out.println(a - b);
+	}
+
+	// Example of Stash - Divide Method
+	public void divide(int a, int b) {
+		System.out.println(a / b);
 	}
 
 }
